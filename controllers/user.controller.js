58c821094110821613ef45115002ed6e7d21c5a5
JhonFaverM <91922585+JhonFaverM@users.noreply.github.com>
2021-11-17 17:29:19 -0500

    mensaje

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,50 @@
 const ctrluser = {},
   User = require("../models/user");
+
 ctrluser.create = async (req, res) => {
   const newuser = new User({
     user: req.body.user,
     status: req.body.status
-
-    //date: new Date (req.body.date), //debe mandarse asi: 2021-11-20
-    //time: new Date("1970-01-01 " + req.body.time), //debe mandarse asi: 21:07:00
-    //patient: req.body.patient_id,
   });
-
-  
   await newuser.save();
-
-  
-
   res.json({
     msg: "User created successfully",
-    
+    status: true  
   });
+};
+ctrluser.list = async (req, res) => {
+  const user = await User.find();
+  //console.log("users", users);
+
+  res.json(user);
+};
+ctrluser.update = async (req, res) => {
+  const { _id, user, } = req.body;
+  await User.findOneAndUpdate(
+    { _id: _id },
+    { 
+      user: user,
+    }
+  );
+  res.json({ status: true });
+};
+ctrluser.delete = async (req, res) => {
+  const { _id } = req.params;
+  const users = await User.deleteOne({ _id: _id });
+  res.json({ status: true });
+};
+ctrluser.search = async (req, res ) => {
+  const user = await User.find( { user : { $regex: ".*" + req.body.user + ".*" } })
+  res.json(user)
+}
+ctrluser.active = async (req, res) => {
+  const { _id, status } = req.body;
+  await User.findOneAndUpdate(
+    { _id: _id },
+    { 
+      status: status
+    }
+  );
+  res.json({ status: true });
 };
 module.exports = ctrluser;
--- route/user.route.js
+++ route/user.route.js
@@ -2,6 +2,13 @@ const express = require('express'),
 router =  express.Router(),
 userctrl = require('../controllers/user.controller')
 
+router.get('/', userctrl.list)
 router.post('/', userctrl.create)
+router.delete('/:_id', userctrl.delete)
+router.put('/', userctrl.update)
+router.put('/active', userctrl.active)
+router.post('/search', userctrl.search)
+router.get('/searchget', userctrl.search)
+
 
 module.exports = router
