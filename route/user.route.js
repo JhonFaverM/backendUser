c22ff5707a65073956e0c9ed6ceca7d2a5516ab3
JhonFaverM <91922585+JhonFaverM@users.noreply.github.com>
2021-11-17 20:30:49 -0500

    cambio router

--- route/user.route.js
+++ route/user.route.js
@@ -8,7 +8,7 @@ router.delete('/:_id', userctrl.delete)
 router.put('/', userctrl.update)
 router.put('/active', userctrl.active)
 router.post('/search', userctrl.search)
-router.get('/searchget', userctrl.search)
+router.get('/searchget', userctrl.searchget)
 
 
 module.exports = router
